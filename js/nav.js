68b3db6ebc2351d00e23502d87d8ce05de677be1
bywillsilva <raniwillere@gmail.com>
2023-10-28 11:40:53 -0300

    Atualização

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+const closeIcon = document.querySelector(".close-icon");
+const openIcon = document.querySelector(".open-icon");
+const body = document.querySelector("body");
+const headerMobile = document.querySelector(".header-mobile");
+
+closeIcon.addEventListener("click", () => {
+    headerMobile.style.display = "none";
+});
+
+openIcon.addEventListener("click", () => {
+    headerMobile.style.display = "flex";
+    body.style.overflow = "hidden";
+});
